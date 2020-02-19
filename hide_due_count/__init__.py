906dae200e13abe59b3861a00d1bef6164d695cc
bluegreenmagick <bluegreenmagick@gmail.com>
2020-02-19 17:10:33 +0900

    remove tooltip debug messages

--- hide_due_count/__init__.py
+++ hide_due_count/__init__.py
@@ -1,9 +1,6 @@
 from aqt.deckbrowser import DeckBrowser
 from anki.hooks import wrap
 
-from aqt.utils import tooltip
-
-
 class magical_question_mark:
 
     def __add__(self, other):
@@ -22,7 +19,6 @@ def myfunc(self, node, *args, _old, **kwargs):
     #node is a tuple
     #name, did, due, lrn, new, children
     a = list(node)
-    tooltip("a")
     if a[2] + a[3]:
         a[2] = magical_question_mark()
         a[4] = magical_question_mark()
